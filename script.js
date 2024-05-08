1c118bc2142c1ff05e13b4c070944808ec15f75b
Viraj Dey <viraj65.dey@gmail.com>
2024-05-08 20:16:18 +0530

    5

--- script.js
+++ script.js
@@ -10,22 +10,23 @@ window.onmouseup = () => {
 }
 
 window.onmousemove = e => {
-    if(track.dataset.mouseDownAt === "0") return;
+    if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta  = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth / 2;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
 
     const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
     track.dataset.percentage = nextPercentage;
 
     track.animate({
         transform: `translate(${nextPercentage}%, -50%)`
-    }, {duration: 1200, fill: "forwards"});
-    
-    for(const image of track.getElementsByClassName("image")) {
+    }, { duration: 1200, fill: "forwards" });
+
+    for (const image of track.getElementsByClassName("image")) {
         image.animate({
             objectPosition: `${100 + nextPercentage}% center`
-        }, {duration: 1200, fill: "forwards"});
+        }, { duration: 1200, fill: "forwards" });
     }
 }
